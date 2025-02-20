c9e9a862ce47e15cc7c4cde74514f05a209aa6f3
Odin <o.schrama@student.vu.nl>
2025-02-20 12:33:54 +0100

    new

--- Fitbit3.py
+++ Fitbit3.py
@@ -1,5 +1,12 @@
 import sqlite3
-connection = sqlite3.connect("fitbit_database.db")
-query = f
+import pandas as pd
 
-                             
+connection = sqlite3.connect("fitbit_database.db")
+cursor = connection.cursor()
+
+var = 60000
+query = "SELECT * FROM employees WHERE salary > ?"
+cursor.execute(query, (var,))
+
+rows = cursor.fetchall()
+df = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
